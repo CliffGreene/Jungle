1a44b34578370d44b7f861413b41fa9e97f62a6d
CliffGreene <lifeguardtwenty@gmail.com>
2020-03-05 00:46:14 +0000

    finishing about page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def show
+    @message = "Welcome to Jungle! The place where shit!"
+  end
+end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+  @categories = Category.count(:id)
+  @products = Product.count(:id)
+
   end
 end
