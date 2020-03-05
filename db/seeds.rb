d0284488841ada532e5adefbbbfb1e26ce4c8e6f
CliffGreene <lifeguardtwenty@gmail.com>
2020-03-05 18:36:08 +0000

    Added admin category section

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# bin/rake db:reset    To reset database
+
 puts "Seeding Data ..."
 
 # Helper functions
